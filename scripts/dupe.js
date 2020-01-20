fa2b51cb3f79deda03f69cfac111976d22e1f35f
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2020-01-20 12:05:39 +0800

    Update dupe.js

--- scripts/dupe.js
+++ scripts/dupe.js
@@ -9,65 +9,60 @@ function convert(entity) {
 const router = Vars.content.getByName(ContentType.block, 'router');
 
 const duper = extendContent(Router, "dupe", {
-	
-	// copy of the original method, modified to use the centralized dictionary
+    update(tile){
+        var entity = tile.ent();
+
+        if(entity.lastItem == null && entity.items.total() > 0){
+            entity.items.clear();
+        }
+
+        if(entity.lastItem != null){
+            entity.time += 1f / speed * Time.delta();
+            var target = getTileTarget(tile, entity.lastItem, entity.lastInput, false);
+
+            if(target != null && (entity.time >= 1f || !(target.block() instanceof Router))){
+                getTileTarget(tile, entity.lastItem, entity.lastInput, true);
+                target.block().handleItem(entity.lastItem, target, Edges.getFacingEdge(tile, target));
+                entity.items.remove(entity.lastItem, 1);
+                entity.lastItem = null;
+            }
+        }
+    }
+
+    acceptItem(item, tile, source){
+        var entity = tile.ent();
+
+        return tile.getTeam() == source.getTeam() && entity.lastItem == null && entity.items.total() == 0;
+    }
+
+    handleItem(item, tile, source){
+        var entity = tile.ent();
+        entity.items.add(item, 1);
+        entity.lastItem = item;
+        entity.time = 0f;
+        entity.lastInput = source;
+    }
+
+    getTileTarget(tile, item, from, set){
+        var[] proximity = tile.entity.proximity();
+        var counter = tile.rotation();
+        for(int i = 0; i < proximity.size; i++){
+            var other = proximity.get((i + counter) % proximity.size);
+            if(set) tile.rotation((byte)((tile.rotation() + 1) % proximity.size));
+            if(other == from && from.block() == Blocks.overflowGate) continue;
+            if(other.block().acceptItem(item, other, Edges.getFacingEdge(tile, other))){
+                return other;
+            }
+        }
+        return null;
+    }
+
     removeStack(tile, item, amount){
-        var entity = convert(tile.ent());
-        var result = router.removeStack(tile, item, amount);
+        var entity = tile.ent();
+        var result = super.removeStack(tile, item, amount);
         if(result != 0 && item == entity.lastItem){
             entity.lastItem = null;
         }
         return result;
-    },
-	
-	// copy of the original method, modified to use the centralized dictionary
-    acceptItem(item, tile, source){
-        var entity = convert(tile.ent());
-
-        return tile.getTeam() == source.getTeam() && entity.lastItem === null && entity.items.total() == 0;
-    },
-	
-	// copy of the original method, modified to use the centralized dictionary
-    handleItem(item, tile, source){
-        var entity = convert(tile.ent());
-        entity.items.add(item, 2);
-        entity.lastItem = item;
-        entity.time = 0;
-        entity.lastInput = source;
-    },
-
-    update(tile) {
-        var entity = convert(tile.ent());
-        if (entity.lastItem === null && entity.items.total() > 0) {
-            //entity.items.clear();
-        }
-        var getTargetAndFlip = (tile, item, src) => {
-            var incomingDirection = tile.relativeTo(src.x, src.y);
-            if (incomingDirection === -1) return null;
-
-            var outputCandidates = [1,2,3,4];
-               
-            var output = outputCandidates
-                .filter((t) => t !== null)
-                .filter((t) => t.block().acceptItem(item, t, tile))
-                [0];
-            if (output === undefined) return null;
-            
-            if (output === outputCandidates[0]) {
-                tile.rotation((!tile.rotation())|0);
-            }
-            return output;
-        }
-		
-		entity.time += 1 / router.speed * Time.delta();
-		
-        if(entity.lastItem !== null && entity.time >= 1){
-            var target = getTargetAndFlip(tile, entity.lastItem, entity.lastInput);
-            if (target === null) return;
-            
-            target.block().handleItem(entity.lastItem, target, tile);
-            entity.items.remove(entity.lastItem, 1);
-            entity.lastItem = null;
-        }
     }
 })
