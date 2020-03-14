945583a119fa897bae0ec79378086cd970f099be
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2020-03-14 17:50:20 +0800

    Update poweredsource.js

--- scripts/poweredsource.js
+++ scripts/poweredsource.js
@@ -1,35 +1,10 @@
-const poweredsource = extendContent(itemSource, “poweredsource”, {
-    update(tile){
-        var entity = tile.ent();
-        if(tile.entity.outputItem == null) {return;}
-        if(tile.entity.cons.valid()){
-            tile.entity.items.set(entity.outputItem, 1);
-            tryDump(tile, tile.entity.outputItem);
-            tile.entity.items.set(entity.outputItem, 0);
-        }
+const poweredsource=extendContent(ItemSource,"poweredsource",{
+  update(tile){
+    var entity=tile.ent();
+    if(tile.entity.cons.valid()){
+      this.super$update(tile);
+      entity.cons.trigger();
     }
-    
-    outputsItems(){
-        return true;
-    }
-
-    draw(tile){
-        this.super$draw(tile);
-
-        var entity = tile.ent();
-        if(tile.entity.outputItem == null) return;
-
-        Draw.color(tile.entity.outputItem.color);
-        Draw.rect("center", tile.worldx(), tile.worldy());
-        Draw.color();
-    }
-
-    buildConfiguration(tile, table){
-        var entity = tile.ent();
-        ItemSelection.buildTable(table, content.items(), () => tile.entity.outputItem, item => {
-            lastItem = item;
-            tile.configure(item == null ? -1 : item.id);
-        });
-    }
-
-}
+    else return;
+  }
+});
