fef6f70ba454130c1922e43cdebf34ab4114ff94
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2020-03-14 14:49:12 +0800

    Update poweredsource.js

--- scripts/poweredsource.js
+++ scripts/poweredsource.js
@@ -1,7 +1,7 @@
-const psource = extendContent(itemSource, “poweredsource”, {
+const poweredsource = extendContent(itemSource, “poweredsource”, {
     update(tile){
-        ItemSourceEntity entity = tile.ent()
-        if(entity.outputItem == null) return
+        var entity = tile.ent()
+        if(entity.outputItem == null) {return}
         if(tile.entity.cons.valid()){
             entity.items.set(entity.outputItem, 1)
             tryDump(tile, entity.outputItem)
