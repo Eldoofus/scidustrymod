76fed96c314979b9729fb936da81e3c9f4848699
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2020-03-15 11:39:06 +0800

    Update incin.js

--- scripts/incin.js
+++ scripts/incin.js
@@ -2,7 +2,7 @@ const incin=extendContent(ItemVoid,"incin",{
   update(tile){
     var entity=tile.ent();
     if(tile.entity.cons.valid()){
-      this.super$update(tile);
+      this.super$handleItem(tile, Vars.content.items(1), tile);
       entity.cons.trigger();
     }
     else return;
