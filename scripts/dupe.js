7cc7b5444a37dcf7829e2571701549c48018a67e
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2020-03-15 11:29:33 +0800

    Update dupe.js

--- scripts/dupe.js
+++ scripts/dupe.js
@@ -1,13 +1,21 @@
 const dupe=extendContent(Block,"dupe",{
+  var lastItem = null;
+  handleItem(item, tile){
+        var entity = tile.ent();
+        tile.entity.items.add(item, 1);
+        lastItem = item;
+  }
   update(tile){
     var entity=tile.ent();
-    if(tile.entity.lastItem == null && tile.entity.items.total() > 0){
+    handleItem(item, tile);
+    if(lastItem == null && tile.entity.items.total() > 0){
       tile.entity.items.clear();
     }
     if(tile.entity.lastItem != null){
-      tile.entity.items.add(tile.entity.lastItem, 1);
+      tile.entity.items.add(lastItem, 1);
       tryDump();
-      tile.entity.items.remove(tile.entity.lastItem, 1);
+      tile.entity.items.remove(lastItem, 1);
+      lastItem = null;
     }
   }
 });
