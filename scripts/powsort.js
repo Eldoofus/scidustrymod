4dda9ca80cd6f23507548eda4d9fb530eaf6b6b3
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2020-04-04 21:03:48 +0800

    Update powsort.js

--- scripts/powsort.js
+++ scripts/powsort.js
@@ -2,9 +2,9 @@ const powsort=extendContent(Sorter,"powsort",{
   acceptItem(item, tile, source){
     var entity=tile.ent();
     if(tile.entity.cons.valid()){
-      this.super$acceptItem(item, tile, source);
+      return this.super$acceptItem(item, tile, source);
       tile.entity.cons.trigger();
     }
-    else return;
+    else return false;
   }
 });
