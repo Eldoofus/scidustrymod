2fab9586176ee57ec63413c2f1ab5310d680d548
Eldoofus339 <49511921+Eldoofus339@users.noreply.github.com>
2020-04-09 14:34:59 +0800

    Update switchpass.js

--- scripts/switchpass.js
+++ scripts/switchpass.js
@@ -1,14 +1,24 @@
 const switchpass=extendContent(Sorter,"switchpass",{
-  update(tile){
-    var invert = this.invert;
-    var entity = tile.ent();
+  // update(tile){
+  //   var invert = this.invert;
+  //   var entity = tile.ent();
+  //   if(tile.entity.cons.valid()){
+  //     invert = true;
+  //     entity.cons.trigger();
+  //   }
+  //   else {
+  //     invert = false;
+  //   }
+  //   return;
+  // }
+  getTileTarget(item, dest, source, flip){
     if(tile.entity.cons.valid()){
-      invert = true;
       entity.cons.trigger();
+      return this.super$getTileTarget(item, dest, source, !(flip));
     }
     else {
-      invert = false;
+      return this.super$getTileTarget(item, dest, source, flip);
     }
-    return;
+    
   }
 });
