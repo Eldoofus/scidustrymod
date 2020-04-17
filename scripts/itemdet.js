23646eb3bb66688243b27923a466026ef620c7d1
Eldoofus339 <49511921+Eldoofus339@users.noreply.github.com>
2020-04-17 08:02:23 +0800

    change stuff

--- scripts/itemdet.js
+++ scripts/itemdet.js
@@ -12,6 +12,10 @@ const itemdet=extendContent(Sorter,"itemdet",{
         // } else {
         //     return 0;
         // }
+    },
+    setBars(){
+        this.super$setBars();
+        this.bars.add("poweroutput",func(entity=>new Bar(prov(()=>Core.bundle.format("bar.poweroutput",entity.block.getPowerProduction(entity.tile)*60*entity.timeScale)),prov(()=>Pal.powerBar),floatp(()=>entity.tile.entity!=null?entity.block.getPowerProduction(entity.tile):0))));
     }
   });  
 itemdet.entityType=prov(()=>extendContent(Sorter.SorterEntity,itemdet,{
