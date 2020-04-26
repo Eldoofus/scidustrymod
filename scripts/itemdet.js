d6900c4c4705b9a56c35ec45c83ab45eb72aaedc
Eldoofus339 <49511921+Eldoofus339@users.noreply.github.com>
2020-04-26 16:32:18 +0800

    Update itemdet.js

--- scripts/itemdet.js
+++ scripts/itemdet.js
@@ -1,5 +1,8 @@
 const presstick=8; const timerid=0;
 const itemdet=extendContent(Sorter,"itemdet",{
+    isSame(other){
+        return false;
+    },
     placed(tile) {
         this.super$placed(tile);
         tile.ent().timer.reset(timerid,presstick+1);
