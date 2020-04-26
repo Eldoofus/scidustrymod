421833b4b680bc13307d6e1ccde6d7b6c3ff2525
Eldoofus339 <49511921+Eldoofus339@users.noreply.github.com>
2020-04-26 16:05:55 +0800

    Update itemdet.js

--- scripts/itemdet.js
+++ scripts/itemdet.js
@@ -1,32 +1,49 @@
+const presstick=10; const timerid=0;
 const itemdet=extendContent(Sorter,"itemdet",{
-    acceptItem(item,tile,source){
-        if(this.super$acceptItem(item,tile,source)){
-          tile.entity.setProp(true);
-        }
-        return this.super$acceptItem(item,tile,source);
+    placed(tile) {
+        this.super$placed(tile);
+        tile.ent().timer.reset(timerid,presstick+1);
+    },
+    draw(tile) {
+        Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerid,presstick)) ? "":"-trig")), tile.drawx(), tile.drawy());
+    },
+    acceptItem(tile){
+        var entity=tile.ent();
+        if(tile.entity.items.total()>0) tile.ent().timer.reset(timerid,0);
+        return this.super$acceptItem(tile);
     },
     getPowerProduction(tile){
-        if(tile.entity.getProp() || _tick < 2){
-            tile.entity.setProp(false);
-            _tick += 1;
-            return 1;
-        } else {
-            _tick = 0;
-            return 0;
-        }
-    },
-    setBars(){
-        this.super$setBars();
-        this.bars.add("poweroutput",func(entity=>new Bar(prov(()=>Core.bundle.format("bar.poweroutput",entity.block.getPowerProduction(entity.tile)*60*entity.timeScale)),prov(()=>Pal.powerBar),floatp(()=>entity.tile.entity!=null?entity.block.getPowerProduction(entity.tile):0))));
+      return (tile.ent().timer.check(timerid,presstick)) ? 0: 1;
     }
-  });  
-itemdet.entityType=prov(()=>extendContent(Sorter.SorterEntity,itemdet,{
-    getProp(){
-        return this._prop;
-    },
-    setProp(a){
-        this._prop=a;
-    },
-    _prop:false,
-    _tick:0,
-}))
+})
+//     acceptItem(item,tile,source){
+//         if(this.super$acceptItem(item,tile,source)){
+//           tile.entity.setProp(true);
+//         }
+//         return this.super$acceptItem(item,tile,source);
+//     },
+//     getPowerProduction(tile){
+//         if(tile.entity.getProp() || (_tick < 2)){
+//             tile.entity.setProp(false);
+//             _tick += 1;
+//             return 1;
+//         } else {
+//             _tick = 0;
+//             return 0;
+//         }
+//     },
+//     setBars(){
+//         this.super$setBars();
+//         this.bars.add("poweroutput",func(entity=>new Bar(prov(()=>Core.bundle.format("bar.poweroutput",entity.block.getPowerProduction(entity.tile)*60*entity.timeScale)),prov(()=>Pal.powerBar),floatp(()=>entity.tile.entity!=null?entity.block.getPowerProduction(entity.tile):0))));
+//     }
+//   });  
+// itemdet.entityType=prov(()=>extendContent(Sorter.SorterEntity,itemdet,{
+//     getProp(){
+//         return this._prop;
+//     },
+//     setProp(a){
+//         this._prop=a;
+//     },
+//     _prop:false,
+//     _tick:0,
+// }))
