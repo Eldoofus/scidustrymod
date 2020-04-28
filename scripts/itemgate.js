f7717cab4b52e141042909d6ac596e0b04080838
Eldoofus339 <49511921+Eldoofus339@users.noreply.github.com>
2020-04-28 08:07:24 +0800

    change stuff

--- scripts/itemgate.js
+++ scripts/itemgate.js
@@ -1,15 +1,15 @@
-const presstick=8; const timerid=0;
+//const presstick=8; const timerid=0;
 const itemgate=extendContent(OverflowGate,"itemgate",{
-    placed(tile) {
-        this.super$placed(tile);
-        tile.ent().timer.reset(timerid,presstick+1);
-    },
-    update(tile){
-        var entity=tile.ent();
-        if(tile.entity.items.total()>0) entity.timer.reset(timerid,0);
-        this.super$update(tile);
-    },
+    // placed(tile) {
+    //     this.super$placed(tile);
+    //     tile.ent().timer.reset(timerid,presstick+1);
+    // },
+    // update(tile){
+    //     var entity=tile.ent();
+    //     if(tile.entity.items.total()>0) entity.timer.reset(timerid,0);
+    //     this.super$update(tile);
+    // },
     getPowerProduction(tile){
-      return (tile.ent().timer.check(timerid,presstick)) ? 0: 1;
+      return tile.entity.items.total()>0;
     }
 })
