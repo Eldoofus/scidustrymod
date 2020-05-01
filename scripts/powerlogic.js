a5b41116c970cb060fcf998188f8bcae7e8c588c
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-01 16:29:07 +0900

    what?

--- scripts/powerlogic.js
+++ scripts/powerlogic.js
@@ -188,10 +188,10 @@ const powerlogic=extendContent(MessageBlock,"powerlogic",{
       return Math.floor(value/8)%2+"-"+Math.floor(value/4)%2+"-"+Math.floor(value/2)%2+"-"+Math.floor(value)%2;
     },
     drawRequestConfig(req, list){
-      var logicshape=this.numtostr(req.config);
+      //var logicshape=this.numtostr(req.config);
       Draw.rect(Core.atlas.find(this.name+"-base"), req.drawx(), req.drawy());
       Draw.rect(Core.atlas.find(this.name+"-top"), req.drawx(), req.drawy(),90*req.rotation);
-      Draw.rect(Core.atlas.find(this.name+"-"+logicshape), req.drawx(), req.drawy(),90*req.rotation);
+      Draw.rect(Core.atlas.find(this.name+"-"+req.config), req.drawx(), req.drawy(),90*req.rotation);
         //this.drawRequestConfigCenter(req, Vars.content.item(req.config), Core.atlas.find("center"));
     }
     //TODO:table, draw
