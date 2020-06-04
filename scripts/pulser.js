d21d02d17cb7ba58e0c3ec2009867b7afdfa1ea7
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-06-04 15:52:30 +0900

    wtf

--- scripts/pulser.js
+++ scripts/pulser.js
@@ -1,7 +1,7 @@
 const timerid=0; const maxPulse=360;
 const pulser=extendContent(PowerBlock,"pulser",{
     getPowerProduction(tile){
-        return Mathf.num(tile.ent().timer.get(timerid,tile.ent().getPulse()));
+        return Mathf.num(tile.ent().timer.check(timerid,tile.ent().getPulse()));
     }
 });
 pulser.entityType=prov(() => extend(TileEntity, {
