b279c43692d597ba0cec8b45db72e83b1f39fac9
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-04 16:09:35 +0900

    save last output

--- scripts/pulser.js
+++ scripts/pulser.js
@@ -1,15 +1,35 @@
-const timerid=0; const maxPulse=360;
+const timerid=0; const pulseid=1; const maxPulse=360;
 const pulser=extendContent(PowerBlock,"pulser",{
-    getPowerProduction(tile){
-        return (tile.ent().timer.get(timerid,tile.ent().getPulse()))?1:0;
-    }
+  getPowerProduction(tile){
+    if(tile.ent().timer.getTime(timerid)<=0) return (tile.ent().getLastOutput())?1:0;
+    tile.ent().timer.reset(timerid,0);
+    var res=tile.ent().timer.get(pulseid,tile.ent().getPulse());
+    tile.ent().setLastOutput(res);
+    return (res)?1:0;
+  }
 });
+
 pulser.entityType=prov(() => extend(TileEntity, {
-    _pulse:60,
-    getPulse(){
-        return this._pulse;
-    },
-    setPulse(a){
-        this._pulse = a;
-    }
+  _pulse:60,
+  getPulse(){
+    return this._pulse;
+  },
+  setPulse(a){
+    this._pulse = a;
+  },
+  write(stream){
+    this.super$write(stream);
+    stream.writeShort(this._pulse);
+  },
+  read(stream,revision){
+    this.super$read(stream,revision);
+    this._pulse=stream.readShort();
+  },
+  getLastOutput(){
+    return this._last;
+  },
+  setLastOutput(a){
+    this._last=a;
+  },
+  _last:false
 }));
